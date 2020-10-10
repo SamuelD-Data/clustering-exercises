57d522e2af1db5799c7914f7a90978245c30102b
Samuel Davila <samuelrdavila@gmail.com>
2020-10-10 13:53:38 -0500

    added 2 functions from exercises to py file

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -31,3 +31,34 @@ def all_2017_zillow_data():
                 '''
     df = pd.read_sql(sql_string, get_connection('zillow'))
     return df
+
+def missing_rows(df):
+    """
+    Function creates df which holds each column of passed df as a row and the number of missing rows and percent of missing rows in each of the passed df's rows as columns
+    """
+    # taking sum of missing rows for each variable, multiplying by 100 then dividing by total 
+    # number of rows in original df to find % of missing rows 
+    missing_row_percent = df.isnull().sum() * 100 / len(df)
+    # count number of missing values for each variable and sum for each
+    missing_row_raw = df.isnull().sum()
+    # creating df using series' created by 2 previous variables
+    missing_df = pd.DataFrame({'num_rows_missing' : missing_row_raw, 'pct_rows_missing': missing_row_percent})
+    # return df
+    return missing_df
+
+def missing_cols(df):
+    """
+    Function creates a DF that holds the number of missing columns and percent of missing columns of a passed DF
+    """
+    # df.isna() displays original df frame with true or false for each value as to whether the value is null
+    # .any() creates a series and for each column, shows true if there are any nulls within it (since its being used on the .isna DF)
+    # df.loc[ : ] ~~~~~~~~ .count() means we're looking at every row to count the number of null values in each row 
+    # using the rest of the code explained above
+    num_cols_missing = df.loc[:, df.isna().any()].count()
+    # dividing the counts above by the length of the index of the dataframe (ie. the number of rows)
+    # to get the percent of missing rows and then rounding to 3 decimal places
+    pct_cols_missing = round(num_cols_missing / len(df.index),3)
+    # creating dataframe using series' from above
+    missing_cols_df = pd.DataFrame({'num_cols_missing': num_cols_missing, 'pct_cols_missing': pct_cols_missing})
+    # returning DF
+    return missing_cols_df
